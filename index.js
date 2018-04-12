fcefcc7d647d98bd434e740977783cc8f589f068
JohnGit54 <johnp701@yahoo.com>
2018-04-12 18:54:08 -0400

    added restart logic

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ var Word = require('./word.js');
 var inquirer = require("inquirer");
 
 const START_COUNT = 10;
-var randomWordArr = ['yes', 'no', 'silly',
-    'nonsense', 'tired', 'pillow', 'auto',
-'Knights in White Satin',
+var randomWordArr = ['yes', 'no', 'silly', 'javascript',
+    'nonsense', 'tired', 'pillow', 'auto', 'better call saul',
+    'Knights in White Satin', 'guardians of the galaxy',
+    'forrest gump', 'who wants to be a millionaire',
+    'mary poppins', 'breaking bad', 'microsoft windows'
 ];
 
 var _Word;
@@ -35,21 +37,36 @@ function playGame() {
         //console.log(" playGame function answer", answers.input);
         _Word.guessLetter(answers.input);
         _Word.getLetterDisplay();
-        //handleAnswer(answers.input);
         guessCnt--;
-        //console.log( guessCnt,"bottom of function answer");
 
         if (guessCnt <= 0) {
             console.log('Sorry - you lost');
+            reStartGame();
         } else if (_Word.isWon) {
             console.log("You Won");
+            reStartGame();
         } else {
             playGame();
         }
-
     })
+}
 
-
+function reStartGame() {
+    inquirer.prompt([{
+        name: "start",
+        type: 'checkbox',
+        choices: ["NewGame", "Exit"],
+        message: "Do you want to start new game?"
+    }
+    ]).then(answers => {
+        console.log(answers.start[0]);
+        if (answers.start[0] == 'Exit') {
+            console.log("GoodBye!")
+        } else {
+            initialize();
+            playGame();
+        }
+    });
 }
 
 
